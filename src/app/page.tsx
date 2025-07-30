e702111ec627967e82c59bd4337acd1e8fb45e71
Ridgal <planetstoun@mail.ru>
2025-07-31 00:29:17 +0300

    connect shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
 function Home() {
-  return <div className='text-3xl font-bold underline'>Главная страница</div>;
+  return (
+    <div className='text-3xl font-bold underline'>
+      <Button variant='link'>Outline</Button>
+      <Input type='email' placeholder='Email' />
+      <div className='h-10 w-10 bg-red-500'></div>
+    </div>
+  );
 }
 
 export default Home;
---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss';
+import tailwindAnimate from 'tailwindcss-animate';
+
+const config: Config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {},
+  plugins: [tailwindAnimate],
+};
+export default config;
